3f1db621b9ffe91667b76a2236591deed0c0518f
Atmanand Gauns <83651531+tu2-atmanand@users.noreply.github.com>
2023-03-23 20:14:06 +0530

    Changed namespce to std

--- Mainhead.cpp
+++ Mainhead.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-using namespace std;
-int main()
-  {
-      cout<<"In main to launch other programs"<<endl;
+// using namespace std;
+int main() {
+      std::cout<<"In main to launch other programs"<<endl;
+//   Use always std instead using namespace
+//   TODO : Use the system shell for this task to run other programs, you can use it by including the header file. Ill  do that if you cant
       return 0;
   
   }
